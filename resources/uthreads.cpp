3b01aff8218a43feb8fb451c3d6b32f009256da7
reph200 <raphael.klotnick.43@gmail.com>
2023-04-24 01:55:40 +0300

    finished terminate

--- resources/uthreads.cpp
+++ resources/uthreads.cpp
@@ -10,6 +10,16 @@ using namespace std;
 typedef unsigned int address_t;
 #define JB_SP 4
 #define JB_PC 5
+address_t translate_address(address_t addr)
+{
+  address_t ret;
+  asm volatile("xor    %%gs:0x18,%0\n"
+               "rol    $0x9,%0\n"
+  : "=g" (ret)
+  : "0" (addr));
+  return ret;
+}
+sigjmp_buf env[MAX_THREAD_NUM];
 
 int general_quantom_size;
 int running_remaning_quantom;
@@ -32,8 +42,11 @@ struct Thread
     Thread_State state;
     char *sp;
 
-} Thread;
-
+};
+list <Thread> *thread_list;
+list <Thread> *ready_list;
+Thread *running_thread;
+Thread main_thread;
 void setup_thread (int tid, char *stack, thread_entry_point entry_point)
 {
   // initializes env[tid] to use the right stack, and to run from the function 'entry_point', when we'll use
@@ -46,12 +59,9 @@ void setup_thread (int tid, char *stack, thread_entry_point entry_point)
   sigemptyset (&env[tid]->__saved_mask);
 }
 
-list <Thread> *thread_list;
-list <Thread> *ready_list;
-Thread *running_thread;
-Thread main_thread;
 
-id = 0;
+
+int id = 0;
 int get_available_id ()
 {
   if(available_ids.empty())
@@ -85,7 +95,7 @@ int uthread_spawn (thread_entry_point entry_point)
   }
   char *stack = new char[STACK_SIZE];
   setup_thread (id, stack, entry_point);
-  Thread * new_thread = new Thread (id, Ready, stack);
+  Thread * new_thread = new Thread {id, Ready, stack};
   thread_list->push_back (new_thread);
   ready_list->push_back (new_thread);
   return id;
@@ -93,21 +103,22 @@ int uthread_spawn (thread_entry_point entry_point)
 
 int uthread_terminate (int tid)
 {
-  auto target_thread = std::find_if (thread_list.begin (), thread_list.end (),
+  auto target_thread = std::find_if (thread_list->begin (), thread_list->end
+  (),
                                      [&] (const Thread &t)
                                      { return t.id == tid; });
-  if (target_thread != thread_list.end ())
+  if (target_thread != thread_list->end ())
   {
     return -1;
   }
   else if (tid == 0)
   {
-    for (const auto& thread : thread_list)
+    for (auto& thread : *thread_list)
     {
       ready_list->remove (thread);
       thread_list->remove (thread);
       delete[] thread.sp;
-      delete thread
+      delete thread;
     }
     delete ready_list;
     delete thread_list;
@@ -115,10 +126,10 @@ int uthread_terminate (int tid)
   }
   else
   {
-    new_available_id = target_thread->id;
+    int new_available_id = target_thread->id;
     ready_list->remove (target_thread);
     thread_list->remove (target_thread);
-    delete[] target_thread.sp;
+    delete[] target_thread->sp;
     delete target_thread;
     auto it = std::lower_bound(available_ids.begin(), available_ids.end(), nwe_available_id);
     available_ids.insert(it, new_available_id);
@@ -142,7 +153,7 @@ int uthread_block (int tid)
   auto target_thread = std::find_if(thread_list.begin(), thread_list.end(),
                          [&](const Thread& t) { return t.id == tid; });
 
-  if (target_thread != thread_list.end()) {
+  if (target_thread != thread_list->end()) {
     if(target_thread->state == Blocked)
     {
       return 0;

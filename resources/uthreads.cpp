23f52deb01fcc52cd09c78fda0439f9b1ad94ee9
reph200 <raphael.klotnick.43@gmail.com>
2023-04-23 22:54:14 +0300

    נודר קומטטתי

--- resources/uthreads.cpp
+++ resources/uthreads.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
-#include<
-int uthread_init(int quantum_usecs)
+#include<list>
+#include "uthreads.h"
+using namespace std;
+
+int general_quantom_size;
+int running_remaning_quantom;
+enum Thread_State
+{
+    Running, Ready, Blocked
+};
+
+struct Thread
+{
+    int id;
+    Thread_State state;
+
+} Thread;
+
+list <Thread> thread_list;
+list <Thread> ready_list;
+Thread running_thread;
+Thread main_thread;
+main_thread.
+id = 0;
+int uthread_init (int quantum_usecs)
 {
 
 }
